ff3898c4862cb1a361b208f52a0fc4a0c0f7e6f4
Pankesh Bamotra <pankesh.bamotra@gmail.com>
2016-11-08 01:49:16 -0500

    Added util method to fetch fonts

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import h5py
 import os
 import PIL
 import sys
+import string
 
 
 def read_data(filename='../data/fonts.hdf5', verbose=True):
@@ -39,6 +40,38 @@ def grayscale_to_rgb(grayscale_array):
 
     rgb_image = cv2.cvtColor(grayscale_array, cv.CV_GRAY2RGB)
     return PIL.Image.fromarray(255 - rgb_image)
+
+
+def get_font_image_by_char(font_data, char='all', n_subset=0, out='../data/images/'):
+    charset = list(string.ascii_uppercase) + list(string.ascii_lowercase) + map(str, range(10))
+    mapping = {ch: ch_id for (ch_id, ch) in enumerate(charset)}
+
+    if char == 'all':
+        for font_id, font in enumerate(font_data):
+            for ch_id, ch in enumerate(font):
+                img = grayscale_to_rgb(ch)
+                img.save(out + str(font_id) + '_' + str(ch_id) + '.png')
+            if n_subset != 0 and font_id + 1 == n_subset:
+                break
+    else:
+        for font_id, font in enumerate(font_data):
+            ch = font[mapping[char]]
+            img = grayscale_to_rgb(ch)
+            img.save(out + str(font_id) + '_' + ch + '.png')
+            if n_subset != 0 and font_id + 1 == n_subset:
+                break
+
+
+def image_to_64x64(input_image, output_filename='out.png'):
+    """
+    Convert input image from NIST SD 19 to 64x64 RGB image
+
+    :param input_image: image filename
+    :param output_filename: output image filename
+    :return: 3-channel RGB image corresponding to input image
+    """
+    image = PIL.Image.open(input_image).convert('RGB')
+    image.save(output_filename)
 
 
 if __name__ == '__main__':
